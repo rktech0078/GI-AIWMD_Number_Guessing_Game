fd43e760b07ca2273bfe9592cdd36b5992a27241
Abdul Rafay Khan <rafaykhan007852@gmail.com>
2024-03-24 11:01:14 +0500

    I have been done my working about number guessing game

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 3 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessed",
+        type: "number",
+        message: "Please, Guess a Number Between (1 to 3): "
+    }
+]);
+if (answers.userGuessed === randomNumber) {
+    console.log("Congratulations! You are Winner, You Guessed a Correct Number");
+}
+else {
+    console.log("Loser! You are Lose, You Guessed Incorrect Number, Please Try Again");
+}
